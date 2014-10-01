817eea7132ad123af7939d02e290ea6bf8ebb03c
Norm McGarry <norm@byhook.com>
2014-10-01 11:23:32 -0400

    added a debugging plugin.

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -2,6 +2,8 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "org.jshybugger.cordova": "4.5.6"
+}
 // BOTTOM OF METADATA
 });
